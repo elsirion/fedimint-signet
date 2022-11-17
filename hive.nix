de90684e665de74747a35dd358d469a57bf971fd
elsirion <elsirion@protonmail.com>
2022-11-17 05:31:27 +0100

    final demo cfg

--- hive.nix
+++ hive.nix
@@ -41,19 +41,22 @@ in {
   demo-alpha = demo {
     hostName = "alpha";
     hashedPassword = "$6$hzIRKLk8LIKxm0PT$XHEkBRfYy45AW7evbA8lrdMq64mcBwsKjZ6tHzbJgHFaYNgTvoAR6GdQj.XhYmguXGscrTaJLMlAeDG9KrAyt.";
-    isGateway = true;
+    ip = "45.61.184.243";
   };
   demo-bravo = demo {
     hostName = "bravo";
     hashedPassword = "$6$gZIBwt.fHujNo0xY$dLqq.0ae8Pfxe8qYYrKIPC6sK8Jl04AllEcSgLm6fTMiAXzE.cacN4AwEeOsMMHXeHJIrSrvYtIDAVbBy/YDC0";
+    ip = "45.61.186.63";
   };
   demo-charlie = demo {
     hostName = "charlie";
     hashedPassword = "$6$eKwsHabyXiTNQCfn$RM3IYTxOuJW8Zlqoa8bi03CqYP0wFqXCg0c6uN6twUWOunJploYk43WWnvDfC0XeHRht1NmBSMUZOw.zcG5Be.";
+    ip = "45.61.186.157";
   };
   demo-delta = demo {
     hostName = "delta";
     hashedPassword = "$6$k6QJK/nyCbKEhAU.$ZEDh6dP1t7dC0kJTmWZwlumQRrIFvMcJFxb1r1hNo0..dXq3ftfzcNn2/DtvbaKqMqju6ErxK3LRIyrSImOJu0";
+    ip = "45.61.185.226";
   };
 }
 
--- machines/demo.nix
+++ machines/demo.nix
@@ -1,23 +1,27 @@
-{hostName, hashedPassword, isGateway ? false}:
+{hostName, hashedPassword, ip, isGateway ? false}:
 { config, pkgs, lib, ... }:
 let
-  nix-bitcoin = import ./templates/nix-bitcoin.nix;
+  nix-bitcoin = builtins.fetchGit {
+    url = "https://github.com/elsirion/nix-bitcoin/";
+    ref = "adopting";
+    rev = "665542935c7598d6e9751c2ae86b4dbb5cda993d";
+  };
   fedimint-override = pkgs.callPackage
     ({ stdenv, lib, rustPlatform, fetchurl, pkgs, fetchFromGitHub, openssl, pkg-config, perl, clang, jq }:
       rustPlatform.buildRustPackage rec {
         pname = "fedimint";
         version = "master";
-        nativeBuildInputs = [ pkg-config perl openssl clang jq pkgs.mold ];
+        nativeBuildInputs = [ pkg-config perl openssl clang jq pkgs.mold pkgs.llvmPackages.lld ];
         doCheck = false;
         OPENSSL_DIR = "${pkgs.openssl.dev}";
         OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";  
         LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
         src = builtins.fetchGit {
-          url = "https://github.com/fedimint/fedimint";
-          ref = "hcpp";
-          rev = "10d408e20e99b33e9cca102bf104324cee893134";
+          url = "https://github.com/elsirion/fedimint";
+          ref = "ui-dkg";
+          rev = "ab4db1043c1e33b7ad35c65ba8279b493ce9640e";
         };
-        cargoSha256 = "sha256-rFkzIvW11DmDEn3QpcAUS7owqU8iTnjX0uTBGUlvtyc=";
+        cargoSha256 = "sha256-7LToch7VaOECgc4EDEyP2WNw+llHANilU3XJIyrqy6o=";
         meta = with lib; {
           description = "Federated Mint Prototype";
           homepage = "https://github.com/fedimint/fedimint";
@@ -25,6 +29,19 @@ let
           maintainers = with maintainers; [ wiredhikari ];
         };
       }) {};
+  # fedimint-override = (import
+  #   (
+  #     fetchTarball {
+  #       url = "https://github.com/edolstra/flake-compat/archive/b4a34015c698c7793d592d66adbab377907a2be8.tar.gz";
+  #       sha256 = "sha256:1qc703yg0babixi6wshn5wm2kgl5y1drcswgszh4xxzbrwkk9sv7";
+  #     }
+  #   )
+  #   { src = fetchTarball {
+  #       url = "https://github.com/elsirion/fedimint/archive/5b7e11cab974ebaa569abbde6d9a62fb9f7b2e0e.tar.gz";
+  #       sha256 = "sha256:18f1xh4zp7kf0pr3acjw71mk7zs3365szsi8d2n24sbp5m0230kf";
+  #     };
+  #   }
+  # ).defaultNix.packages.x86_64-linux.workspaceBuild;
   fqdn = "${hostName}.demo.sirion.io";
 in
 {
@@ -41,7 +58,7 @@ in
  
   networking = {
     hostName = hostName;
-    firewall.allowedTCPPorts = [ 80 443 ];
+    firewall.allowedTCPPorts = [ 80 443 5002 5003 5004 5005 5006 5007 5008 5009 5010 5011 8333 ];
     interfaces.ens3.useDHCP = true;
   };
 
@@ -80,6 +97,16 @@ in
       enable = true;
       regtest = true;
       dbCache = 1000;
+      extraConfig = ''
+        connect=alpha.demo.sirion.io:8333
+        connect=bravo.demo.sirion.io:8333
+        connect=charlie.demo.sirion.io:8333
+        connect=delta.demo.sirion.io:8333
+        rpcauth=bitcoin:a15feeea5b0ec69c22a6ba065816c591$e0822ecb0e7b36c9c77ec8aae3889d6fd3323e8fdac8cbd06cf8f83734130fc5
+        fallbackfee=0.00008
+      '';
+      address = "0.0.0.0";
+      listen = true;
     };
 
     fedimint = {
@@ -128,7 +155,26 @@ in
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://104.244.73.68:5000";
+          proxyPass = "http://${ip}:5003";
+          proxyWebsockets = true;
+          extraConfig = "proxy_pass_header Authorization;";
+        };
+        locations."/admin/" = {
+          proxyPass = "http://127.0.0.1:5001/";
+          proxyWebsockets = true;
+          extraConfig = "proxy_pass_header Authorization;";
+        };
+        locations."/tty/" = {
+          proxyPass = "http://127.0.0.1:7681/";
+          proxyWebsockets = true;
+          extraConfig = "proxy_pass_header Authorization;";
+        };
+      };
+      virtualHosts."admin.${fqdn}" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:5001/";
           proxyWebsockets = true;
           extraConfig = "proxy_pass_header Authorization;";
         };
